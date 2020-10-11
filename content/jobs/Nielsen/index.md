3e470ac00834ab6939af6bfc2d8383629c34afc1
Abhinash Pati <abhinashpati143@gmail.com>
2020-10-11 10:58:12 +0100

    init commit

---
+++ content/jobs/Nielsen/index.md
@@ -0,0 +1,12 @@
+---
+date: '2018-05-14'
+title: 'Senior Executive'
+company: 'Nielsen'
+location: 'Chennai, India'
+range: 'Dec 2017 - Aug 2019'
+url: 'https://www.upstatement.com/'
+---
+
+- Write modern, performant, maintainable code for a diverse array of client and internal projects
+- Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Gatsby, React, Craft, WordPress, Prismic, and Netlify
+- Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis
---
+++ src/components/sections/about.js
@@ -0,0 +1,182 @@
+import React, { useEffect, useRef } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+import { srConfig } from '@config';
+import sr from '@utils/sr';
+
+const StyledAboutSection = styled.section`
+  max-width: 900px;
+
+  .inner {
+    display: grid;
+    grid-template-columns: 3fr 2fr;
+    grid-gap: 50px;
+
+    @media (max-width: 768px) {
+      display: block;
+    }
+  }
+`;
+const StyledText = styled.div`
+  ul.skills-list {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(140px, 200px));
+    padding: 0;
+    margin: 20px 0 0 0;
+    overflow: hidden;
+    list-style: none;
+
+    li {
+      position: relative;
+      margin-bottom: 10px;
+      padding-left: 20px;
+      font-family: var(--font-mono);
+      font-size: var(--fz-xs);
+
+      &:before {
+        content: '▹';
+        position: absolute;
+        left: 0;
+        color: var(--green);
+        font-size: var(--fz-sm);
+        line-height: 12px;
+      }
+    }
+  }
+`;
+const StyledPic = styled.div`
+  position: relative;
+  max-width: 300px;
+
+  @media (max-width: 768px) {
+    margin: 50px auto 0;
+    width: 70%;
+  }
+
+  .wrapper {
+    ${({ theme }) => theme.mixins.boxShadow};
+    display: block;
+    position: relative;
+    width: 100%;
+    border-radius: var(--border-radius);
+    background-color: var(--green);
+
+    &:hover,
+    &:focus {
+      background: transparent;
+      outline: 0;
+
+      &:after {
+        top: 15px;
+        left: 15px;
+      }
+
+      .img {
+        filter: none;
+        mix-blend-mode: normal;
+      }
+    }
+
+    .img {
+      position: relative;
+      border-radius: var(--border-radius);
+      mix-blend-mode: multiply;
+      filter: grayscale(100%) contrast(1);
+      transition: var(--transition);
+    }
+
+    &:before,
+    &:after {
+      content: '';
+      display: block;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      border-radius: var(--border-radius);
+      transition: var(--transition);
+    }
+
+    &:before {
+      top: 0;
+      left: 0;
+      background-color: var(--navy);
+      mix-blend-mode: screen;
+    }
+
+    &:after {
+      border: 2px solid var(--green);
+      top: 20px;
+      left: 20px;
+      z-index: -1;
+    }
+  }
+`;
+
+const About = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      avatar: file(sourceInstanceName: { eq: "images" }, relativePath: { eq: "me.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 500, traceSVG: { color: "#64ffda" }) {
+            ...GatsbyImageSharpFluid_withWebp_tracedSVG
+          }
+        }
+      }
+    }
+  `);
+
+  const revealContainer = useRef(null);
+
+  useEffect(() => {
+    sr.reveal(revealContainer.current, srConfig());
+  }, []);
+
+  const skills = ['Java', 'C#', 'Kotlin','Android', 'Xamarin', 'AWS', 'GIT', 'Retrofit', 'RxJava'];
+
+  return (
+    <StyledAboutSection id="about" ref={revealContainer}>
+      <h2 className="numbered-heading">About Me</h2>
+
+      <div className="inner">
+        <StyledText>
+          <div>
+            <p>Hello! I'm Abhinash, a software engineer based in Dublin, Ireland.</p>
+
+            <p>
+              I enjoy creating things that live and grow on the internet, whether that be an Android mobile application,
+              Xamarin mobile application, or anything in between. My goal is to always build products that provide
+              pixel-perfect, performant experiences.
+            </p>
+	<p>
+	I recently completed my post-graduation from{' ' }
+	<a href="#">National College of Ireland, Dublin Ireland</a>.
+	</p>
+            <p>
+              But before that, shortly after graduating from{' '}
+              <a href="https://www.ccis.northeastern.edu">Lovely Professional University</a>, I joined the
+              mobile dev team at <a href="https://www.upstatement.com">MIND Infotech</a> where I work
+              on a wide variety of interesting and meaningful Android and Xamarin projects on a daily basis. Not only that, I was also an active contributor for new research initiatives and directions for the reasearch and development team that explored ideas, technologies and directions to open new business door for the organisation.
+            </p>
+	<p>
+	In 2017, I decided to move up in my career and joined <a href="#">Nielsen</a> where I was responsible for development and maintainance of new modules and features in there exiting survey collection mobile app based on Xamarin Android for South Africa and Indonesia. I proposed and developed a mobile device management app for internal monitoring and quality control for which I was awarded "Simply Execelent" award for the contribution.
+	</p>
+            <p>Here are a few technologies I've been working with recently:</p>
+          </div>
+
+          <ul className="skills-list">
+            {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
+          </ul>
+        </StyledText>
+
+        <StyledPic>
+          <div className="wrapper">
+            <Img fluid={data.avatar.childImageSharp.fluid} alt="Avatar" className="img" />
+          </div>
+        </StyledPic>
+      </div>
+    </StyledAboutSection>
+  );
+};
+
+export default About;
